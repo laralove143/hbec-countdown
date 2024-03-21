fe7e86492dfff668bcdbab10997a9f8064a2da4a
Lara Kayaalp <me@lara.lv>
2024-03-21 04:51:20 +0300

    add header

--- locale.js
+++ locale.js
@@ -1,29 +1,28 @@
 const textEn = {
   title: "HBEC Countdown",
+  headerTitle: "Countdown",
+  langSwitcher: "Türkçe",
 };
 
 const textTr = {
   title: "HBEC Geri Sayım",
+  headerTitle: "Geri Sayım",
+  langSwitcher: "English",
 };
 
 function applyLocale() {
+  let locale = new URLSearchParams(window.location.search).get("lang") || "tr";
+  let text = locale === "en" ? textEn : textTr;
+
+  document.documentElement.lang = locale;
+
+  let langSwitcher = document.getElementById("langSwitcher");
+  langSwitcher.href = "?lang=" + (locale === "en" ? "tr" : "en");
+
   document.querySelectorAll("[data-key]").forEach((elem) => {
     let key = elem.getAttribute("data-key");
-    let locale =
-      new URLSearchParams(window.location.search).get("lang") || "tr";
 
-    document.documentElement.lang = locale;
-
-    let text;
-    if (locale === "en") {
-      text = textEn;
-    } else if (locale === "tr") {
-      text = textTr;
-    }
-
-    if (text[key]) {
-      elem.innerText = text[key];
-    }
+    elem.innerText = text[key];
   });
 }
 
