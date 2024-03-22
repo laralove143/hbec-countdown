4a5e4d4283ccf66cbc1620abb531434ce68d5d1e
Lara Kayaalp <me@lara.lv>
2024-03-22 03:56:47 +0300

    implement countdown

--- locale.js
+++ locale.js
@@ -2,21 +2,34 @@ const textEn = {
   title: "HBEC Countdown",
   headerTitle: "Countdown",
   langSwitcher: "Türkçe",
+  welcomeMessage: "Welcome to HBEC. Good luck!",
+  overdueMessage: "Your time is up! Showing overdue duration.",
   durationPickerHours: "Hours",
   durationPickerMinutes: "Minutes",
   durationPickerSeconds: "Seconds",
   durationPickerStart: "Start",
+  durationPickerPause: "Pause",
+  durationPickerContinue: "Continue",
 };
 
 const textTr = {
   title: "HBEC Geri Sayım",
   headerTitle: "Geri Sayım",
   langSwitcher: "English",
+  welcomeMessage: "HBEC'e hoşgeldiniz. İyi şanslar!",
+  overdueMessage: "Süreniz doldu! Geçirdiğiniz süre gösteriliyor.",
   durationPickerHours: "Saat",
   durationPickerMinutes: "Dakika",
   durationPickerSeconds: "Saniye",
   durationPickerStart: "Başla",
+  durationPickerPause: "Durdur",
+  durationPickerContinue: "Devam Et",
 };
+
+function setInnerText(elem) {
+  const key = elem.getAttribute("data-key");
+  elem.innerText = text[key];
+}
 
 const locale = new URLSearchParams(window.location.search).get("lang") || "tr";
 const text = locale === "en" ? textEn : textTr;
@@ -27,7 +40,18 @@ const langSwitcher = document.getElementById("langSwitcher");
 langSwitcher.href = "?lang=" + (locale === "en" ? "tr" : "en");
 
 for (const elem of document.querySelectorAll("[data-key]")) {
-  const key = elem.getAttribute("data-key");
+  const observer = new MutationObserver((mutations) => {
+    if (
+      mutations.find((mutation) => mutation.attributeName === "data-key") ===
+      undefined
+    ) {
+      return;
+    }
 
-  elem.innerText = text[key];
+    setInnerText(elem);
+  });
+
+  observer.observe(elem, { attributes: true });
+
+  setInnerText(elem);
 }
--- main.js
+++ main.js
@@ -1,6 +1,78 @@
-for (const elem of document.querySelectorAll(".duration-picker-unit input")) {
-  elem.addEventListener("input", () => {
-    const value = parseInt(elem.value, 10);
-    elem.value = String(value < 0 ? 0 : value > 59 ? 59 : value);
+const content = document.getElementById("content");
+const durationPickerStart = document.getElementById("durationPickerStart");
+
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+const durationPickerUnits = [hours, minutes, seconds];
+
+let duration;
+let countdownInterval;
+
+function paddedDurationUnit(unit) {
+  return unit < 10 ? `0${unit}` : unit;
+}
+
+function updateCounter() {
+  duration -= 1;
+
+  if (duration === -1) {
+    content.classList.add("overdue");
+    document
+      .getElementById("welcomeMessage")
+      .setAttribute("data-key", "overdueMessage");
+  }
+
+  let shownDuration = Math.abs(duration);
+  hours.value = paddedDurationUnit(Math.floor(shownDuration / 3600));
+  minutes.value = paddedDurationUnit(Math.floor((shownDuration % 3600) / 60));
+  seconds.value = paddedDurationUnit(shownDuration % 60);
+}
+
+for (const unitElem of durationPickerUnits) {
+  unitElem.addEventListener("input", () => {
+    const value = parseInt(unitElem.value, 10);
+    unitElem.value = String(value <= 0 ? 1 : value > 59 ? 59 : value);
+
+    durationPickerStart.disabled = [hours, minutes, seconds].every((elem) => {
+      return elem.value === "";
+    });
   });
 }
+
+durationPickerStart.addEventListener("click", () => {
+  const dataKey = durationPickerStart.getAttribute("data-key");
+  const pauseKey = "durationPickerPause";
+  const continueKey = "durationPickerContinue";
+
+  if (dataKey === pauseKey) {
+    clearInterval(countdownInterval);
+
+    if (duration < 0) {
+      content.classList.remove("overdue");
+      durationPickerStart.disabled = true;
+
+      return;
+    }
+
+    durationPickerStart.setAttribute("data-key", continueKey);
+    return;
+  }
+
+  if (duration === undefined) {
+    duration =
+      parseInt(hours.value * 3600 + minutes.value * 60 + seconds.value) + 1; // add 1 to wait 1 second to decrement after start
+  }
+
+  for (let elem of durationPickerUnits) {
+    elem.disabled = true;
+  }
+
+  countdownInterval = setInterval(() => {
+    updateCounter();
+  }, 1000);
+
+  updateCounter();
+
+  durationPickerStart.setAttribute("data-key", pauseKey);
+});
